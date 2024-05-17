65c17666cd3959b45adf21fb477f6c97e802bf1a
Faroeq <alves_1044@hotmail.com>
2024-05-17 14:31:07 +0200

    Fix position toggle element

--- src/components/togglemenu/ToggleCol.tsx
+++ src/components/togglemenu/ToggleCol.tsx
@@ -3,7 +3,7 @@ import { CustomProps } from "../Calc";
 function ToggleCol(props: CustomProps) {
   return (
     <>
-      <div className="w-4 h-4">{props.children}</div>
+      <div className="w-1/2 h-4">{props.children}</div>
     </>
   );
 }
